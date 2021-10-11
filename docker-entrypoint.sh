6b4a5997ec705101b3173af8ce6f0cc2b3cc4528
LeeDongMin <admin@313.co.kr>
2021-10-11 16:59:10 +0900

    [SmartCommit] SP-581 #close #time 1h +review SR @dongmin.lee #comment CI > docker entrypoint 처리
    [ Java Service Tree Framework(JSTF)::Backend-Module::All-In-One ]
    [Document] http://www.313.co.kr/confluence/display/WIKI
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-582
    [VersionControl] https://github.com/jstree/Java-Service-Tree-Framework-All-In-One
    [CodeReview] http://www.313.co.kr/fecru/project/SR
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/5694
    [Docker] https://hub.docker.com/u/313devgrp

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
 #packetbeat start
-/usr/local/tomcat/packetbeat-7.4.2-linux-x86_64/packetbeat -e -c /usr/local/tomcat/packetbeat-7.4.2-linux-x86_64/packetbeat.yml &
+/packetbeat-7.4.2-linux-x86_64/packetbeat -e -c /packetbeat-7.4.2-linux-x86_64/packetbeat.yml &
 
 #topbeat start
-/usr/local/tomcat/topbeat-1.3.1-x86_64/topbeat -e -c /usr/local/tomcat/topbeat-1.3.1-x86_64/topbeat.yml &
+/topbeat-1.3.1-x86_64/topbeat -e -c /topbeat-1.3.1-x86_64/topbeat.yml &
 
 #metricbeat start
-/usr/local/tomcat/metricbeat-7.4.2-linux-x86_64/metricbeat -e -c /usr/local/tomcat/metricbeat-7.4.2-linux-x86_64/metricbeat.yml &
+/metricbeat-7.4.2-linux-x86_64/metricbeat -e -c /metricbeat-7.4.2-linux-x86_64/metricbeat.yml &
 
 #heartbeat start
-/usr/local/tomcat/heartbeat-7.4.2-linux-x86_64/heartbeat -e -c /usr/local/tomcat/heartbeat-7.4.2-linux-x86_64/heartbeat.yml &
+/heartbeat-7.4.2-linux-x86_64/heartbeat -e -c /heartbeat-7.4.2-linux-x86_64/heartbeat.yml &
 
 #filebeat start
-/usr/local/tomcat/filebeat-7.4.2-linux-x86_64/filebeat -e -c /usr/local/tomcat/filebeat-7.4.2-linux-x86_64/filebeat.yml &
+/filebeat-7.4.2-linux-x86_64/filebeat -e -c /filebeat-7.4.2-linux-x86_64/filebeat.yml &
 
-#tomcat start
-/usr/local/tomcat/bin/catalina.sh run
+set -e
+
+GC_OPTS=${GC_OPTS:="-XX:+UseNUMA -XX:+UseG1GC"}
+MEM_OPTS=${MEM_OPTS:="-Xms4096m -Xmx4096m"}
+NET_OPTS=${NET_OPTS:="-Dsun.net.inetaddr.ttl=0 -Dsun.net.inetaddr.negative.ttl=0 -Djava.net.preferIPv4Stack=true"}
+MONITOR_SCOUTER_OPTS="-javaagent:/scouter.agent.jar -Dscouter.config=/scouter.conf -Dobj_name=JSTF-Allinone"
+MONITOR_ELK_OPTS="-javaagent:/elastic-apm-agent.jar -Delastic.apm.service_name=JSTF-Allinone -Delastic.apm.application_packages=proxy.api -Delastic.apm.server_urls=http://313.co.kr:8200"
+JVM_OPTS="-server $GC_OPTS $MEM_OPTS $NET_OPTS"
+
+JAVA_OPTS=${JAVA_OPTS:="-server $GC_OPTS $MEM_OPTS $NET_OPTS $MONITOR_SCOUTER_OPTS $MONITOR_ELK_OPTS "}
+
+#spring boot start
+exec java -Djava.security.egd=file:/dev/./urandom -jar $JAVA_OPTS -Dspring.profiles.active=live javaServiceTreeFramework.jar $@
