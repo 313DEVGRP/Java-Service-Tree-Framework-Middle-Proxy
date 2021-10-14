64939a46f12469d35e3016b1a67d1acc73b2a3c2
LeeDongMin <admin@313.co.kr>
2021-10-14 22:44:28 +0900

    [SmartCommit] SP-581 #comment Swagger 2 적용 #close #time 1h +review SR @dongmin.lee
    [ Java Service Tree Framework(JSTF)::Backend-Module::All-In-One ]
    [Document] http://www.313.co.kr/confluence/display/WIKI
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-582
    [VersionControl] https://github.com/jstree/Java-Service-Tree-Framework-All-In-One
    [CodeReview] http://www.313.co.kr/fecru/project/SR
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/5694
    [Docker] https://hub.docker.com/u/313devgrp

--- src/main/java/proxy/api/Application.java
+++ src/main/java/proxy/api/Application.java
@@ -3,8 +3,10 @@ package proxy.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
