3f63301fc75c6f87ff8ff70837d1c7b5105a21ca
LeeDongMin <admin@313.co.kr>
2022-11-08 20:58:40 +0900

    [SP-313] #comment 인증. 인가. 프록시 세션 타임 아웃 설정 변경 #close #time 1h +review SR @dongmin.lee
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- src/main/java/proxy/api/config/RedisSessionConfig.java
+++ src/main/java/proxy/api/config/RedisSessionConfig.java
@@ -1,22 +1,18 @@
 package proxy.api.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600) /* 세션 만료 시간 : 600초 */
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800) /* 세션 만료 시간 : 600초 */
 public class RedisSessionConfig {
     @Value("${spring.redis.host}")
     public String host;
