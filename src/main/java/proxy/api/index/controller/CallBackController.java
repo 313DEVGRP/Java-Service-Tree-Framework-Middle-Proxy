98b4890f7490005f7fa510df81ff62122015c40d
LeeDongMin <admin@313.co.kr>
2022-12-21 17:40:44 +0900

    [SP-313] #comment 사용자 정보 가져오는 API 추가 22.12.21 #close #time 1h +review SR @dongmin.lee
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- src/main/java/proxy/api/index/controller/CallBackController.java
+++ src/main/java/proxy/api/index/controller/CallBackController.java
@@ -10,6 +10,7 @@ import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 import proxy.api.scheduler.ArmsRemoteScheduler;
@@ -26,12 +27,14 @@ public class CallBackController {
     private String backendURL;
 
     @RequestMapping(value = "/callback/api/arms/armsScheduler/forceExec/set_PdServiceVersion_toJiraProjectVersion.do", method = RequestMethod.GET)
+    @ResponseBody
     public String set_PdServiceVersion_toJiraProjectVersion() throws Exception {
         armsRemoteScheduler.set_PdServiceVersion_toJiraProjectVersion();
         return "CallForwardBackendController :: set_PdServiceVersion_toJiraProjectVersion";
     }
 
     @RequestMapping(value = "/callback/api/arms/reqStatus/{reqStatusTableName}/updateStatusNode.do", method = RequestMethod.GET)
+    @ResponseBody
     public String putJiraIssue(@PathVariable("reqStatusTableName") String reqStatusTableName) throws Exception {
         logger.info("ArmsScheduler :: putJiraIssue");
         String targetUrl = backendURL + "/auth-anon/api/arms/reqStatus/"+ reqStatusTableName + "/updateStatusNode.do";
@@ -42,7 +45,20 @@ public class CallBackController {
         return "CallForwardBackendController :: putJiraIssue";
     }
 
+    @RequestMapping(value = "/callback/api/arms/reqStatus/{reqStatusTableName}/reqIssueDisable/updateStatusNode.do", method = RequestMethod.GET)
+    @ResponseBody
+    public String updateReqIssueDisable(@PathVariable("reqStatusTableName") String reqStatusTableName) throws Exception {
+        logger.info("ArmsScheduler :: updateReqIssueDisable");
+        String targetUrl = backendURL + "/auth-anon/api/arms/reqStatus/"+ reqStatusTableName + "/reqIssueDisable/updateStatusNode.do";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(targetUrl, String.class);
+        logger.info("response = " + response);
+        return "CallForwardBackendController :: updateReqIssueDisable";
+    }
+
     @RequestMapping(value = "/callback/api/arms/reqStatus/{reqStatusTableName}/issueCrawler/updateStatusNode.do", method = RequestMethod.GET)
+    @ResponseBody
     public String updateJiraIssueCrawler(@PathVariable("reqStatusTableName") String reqStatusTableName) throws Exception {
         logger.info("ArmsScheduler :: updateJiraIssueCrawler");
         String targetUrl = backendURL + "/auth-anon/api/arms/reqStatus/"+ reqStatusTableName + "/issueCrawler/updateStatusNode.do";
