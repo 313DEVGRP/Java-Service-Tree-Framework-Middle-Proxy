22d8bbefa450ff538b0f69bda6827571124c953f
sevoon0909 <sevoon0909@icloud.com>
2023-09-06 22:29:46 +0900

    feat: [ARMSQA-19] #comment 1. auth-user/me 404 에러 추적 임시 커밋 #close #time 1h +review SR @mingyu09
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/arms/config/security/SecurityConfiguration.java
+++ src/main/java/arms/config/security/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package arms.config.security;
 
 import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -11,18 +14,29 @@ import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpe
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.web.server.DefaultServerRedirectStrategy;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+
+import arms.config.handler.KeycloakLogoutHandler;
 import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfiguration {
+    @Value("${spring.security.auth.success.redirect-url}")
+    private  String redirectUrl;
 
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    public SecurityConfiguration(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 
@@ -59,13 +73,17 @@ public class SecurityConfiguration {
                 )
                 .and()
                 .oauth2Login()
-                .and()
-                .formLogin()
-                .loginPage("")
+                .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler(redirectUrl))
                 .and()
                 .logout(logout -> logout
-                        .logoutHandler(logoutHandler)
-                        .logoutSuccessHandler(successHandler)
+                    .logoutUrl("/logout")
+                    .logoutHandler(logoutHandler)
+                    .logoutSuccessHandler(((webFilterExchange, authentication) -> {
+                        ServerWebExchange exchange = webFilterExchange.getExchange();
+                        ServerHttpResponse response = exchange.getResponse();
+                        response.setStatusCode(HttpStatus.OK);
+                        return response.setComplete();
+                    }))
                 )
                 .build();
     }
