7772fd2a41d35b222d21185811be56e1c422468b
mingyu09 <mklee@mz.co.kr>
2023-09-06 17:01:06 +0900

    feat: [ARMSQA-19] #comment 1. 키클락 로그아웃 처리 개선 2. 로그인 성공후 리다이렉트 페이지 설정 추가 3. BlockRequestGatewayFilter 기능 추가    -> 허용url만 로그인 없이 접근 하기 위한 기능  - 23.09.06 #close #time 1h +review SR @sevoon0909
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/arms/config/handler/KeycloakLogoutHandler.java
@@ -0,0 +1,52 @@
+package arms.config.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+
+@Component
+@Slf4j
+public class KeycloakLogoutHandler implements ServerLogoutHandler {
+
+  private final WebClient webClient;
+  private final String serverUrl;
+
+  public KeycloakLogoutHandler(WebClient webClient, @Value("${spring.security.oauth2.client.registration.middle-proxy.server-url}")String serverUrl) {
+    this.webClient = webClient;
+    this.serverUrl = serverUrl;
+  }
+
+  @Override
+  public Mono<Void> logout(WebFilterExchange exchange, Authentication authentication) {
+    OidcUser principal = (OidcUser)authentication.getPrincipal();
+    return logoutFromKeycloak(principal.getIdToken().getTokenValue());
+  }
+
+  private Mono<Void> logoutFromKeycloak(String idToken) {
+    String endSessionEndpoint =  serverUrl+"/realms/master/protocol/openid-connect/logout";//추후에 yml 컨피그로 이동 시켜야함
+    UriComponentsBuilder builder = UriComponentsBuilder
+            .fromUriString(endSessionEndpoint)
+            .queryParam("id_token_hint",idToken);
+
+    return webClient.get()
+            .uri(builder.toUriString())
+            .exchangeToMono(response -> {
+              if (response.statusCode().is2xxSuccessful()) {
+                log.info("Successfully logged out from Keycloak");
+                return Mono.empty();
+              } else {
+                log.error("Could not propagate logout to Keycloak");
+                return Mono.error(new RuntimeException("Could not propagate logout to Keycloak"));
+              }
+            });
+  }
+
+}
--- src/main/java/arms/config/security/SecurityConfiguration.java
+++ src/main/java/arms/config/security/SecurityConfiguration.java
@@ -1,21 +1,22 @@
 package arms.config.security;
 
-import java.net.URI;
+import arms.config.handler.KeycloakLogoutHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
-import org.springframework.security.web.server.DefaultServerRedirectStrategy;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
-import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
-import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -23,52 +24,57 @@ import reactor.core.publisher.Mono;
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfiguration {
 
-  @Bean
-  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 
-    DelegatingServerLogoutHandler logoutHandler = new DelegatingServerLogoutHandler(
-        new WebSessionServerLogoutHandler(),
-        new SecurityContextServerLogoutHandler()
-    );
+    @Value("${spring.security.auth.success.redirect-url}")
+    private  String redirectUrl;
 
-    ServerLogoutSuccessHandler successHandler = (exchange, authentication)
-        -> new DefaultServerRedirectStrategy().sendRedirect(
-              exchange.getExchange()
-            , URI.create("http://www.313.co.kr/auth/realms/master/protocol/openid-connect/logout")
-          );
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
 
-    return http
-              .cors(CorsSpec::disable)
-              .csrf(CsrfSpec::disable)
-              .authorizeExchange(
-                    authorize -> authorize
-                    .pathMatchers("/swagger-ui/**").permitAll()
-                    .pathMatchers("/swagger-resources/**").permitAll()
-                    .pathMatchers("/v2/api-docs/**").permitAll()
-                    .pathMatchers("/dwr/**").permitAll()
-                    .pathMatchers("/auth-anon/**").permitAll()
-                    .pathMatchers("/auth-user/**").hasAnyRole("USER", "ADMIN")
-                    .pathMatchers("/auth-admin/**").hasRole("ADMIN")
-                    .pathMatchers("/auth-check/**").hasAnyRole("USER", "ADMIN")
-                    .anyExchange().authenticated()
-              )
-              .exceptionHandling()
-                .authenticationEntryPoint(
-                    (exchange, denied)
-                        -> Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Session is null"))
+    public SecurityConfiguration(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        DelegatingServerLogoutHandler logoutHandler = new DelegatingServerLogoutHandler(
+                new WebSessionServerLogoutHandler()
+                , keycloakLogoutHandler
+        );
+
+        return http
+                .cors(CorsSpec::disable)
+                .csrf(CsrfSpec::disable)
+                .authorizeExchange(
+                        authorize -> authorize
+                                .pathMatchers("/swagger-ui/**").permitAll()
+                                .pathMatchers("/swagger-resources/**").permitAll()
+                                .pathMatchers("/v2/api-docs/**").permitAll()
+                                .pathMatchers("/auth-anon/**","/dwr/**").permitAll()
+                                .pathMatchers("/auth-user/**","/auth-check/**").hasAnyRole("USER", "ADMIN")
+                                .pathMatchers("/auth-admin/**").hasRole("ADMIN")
+                                .anyExchange().authenticated()
                 )
-              .and()
-              .oauth2Login()
-              .and()
-              .formLogin()
-              .loginPage("")
-              .and()
-              .logout(logout -> logout
-                  .logoutHandler(logoutHandler)
-                  .logoutSuccessHandler(successHandler)
-              )
-        .build();
-  }
-
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (exchange, denied)
+                                -> Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Session is null"))
+                )
+                .and()
+                .oauth2Login()
+                .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler(redirectUrl))
+                .and()
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutHandler(logoutHandler)
+                        .logoutSuccessHandler(((webFilterExchange, authentication) -> {
+                            ServerWebExchange exchange = webFilterExchange.getExchange();
+                            ServerHttpResponse response = exchange.getResponse();
+                            response.setStatusCode(HttpStatus.OK);
+                            return response.setComplete();
+                        }))
+                )
+                .build();
+    }
 
 }
