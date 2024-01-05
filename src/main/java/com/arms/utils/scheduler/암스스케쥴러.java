06a69a42e6595e028487775a5fbea4af52abd785
LeeDongMin <admin@313.co.kr>
2024-01-05 21:52:00 +0900

    feat: [ARMSQA-19] #comment :white_check_mark: 스케쥴러 완료 - 24.01.05 #close #time 1h +review SR @313devops
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/utils/scheduler/암스스케쥴러.java
+++ src/main/java/com/arms/utils/scheduler/암스스케쥴러.java
@@ -20,7 +20,7 @@ public class 암스스케쥴러 {
     private 내부통신기 내부통신기;
 
 
-    @Scheduled(cron="0 40 21 * * *") // 매일 오전 01(새벽)시에 실행
+    @Scheduled(cron="0 0 1 * * *") // 매일 오전 01(새벽)시에 실행
     public void 서버정보백업_스케줄러() {
         log.info("[ 암스스케쥴러 :: 서버정보백업_스케줄러 ] 동작 : {}", Calendar.getInstance().getTime());
         Iterable<서버정보_엔티티> 결과 = 내부통신기.서버정보백업_스케줄러();
