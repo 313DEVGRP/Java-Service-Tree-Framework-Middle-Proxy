41996746855dc2eca02ac3191886bbd210768f85
LeeDongMin <admin@313.co.kr>
2024-01-05 19:43:53 +0900

    feat: [ARMSQA-19] #comment :white_check_mark: 스케쥴러 업데이트 - 24.01.05 #close #time 1h +review SR @313devops
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/utils/scheduler/암스스케쥴러.java
+++ src/main/java/com/arms/utils/scheduler/암스스케쥴러.java
@@ -4,9 +4,11 @@ import com.arms.utils.external_communicate.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Calendar;
 
@@ -22,31 +24,35 @@ public class 암스스케쥴러 {
     private 백엔드코어통신기 백엔드코어통신기;
 
 
-    @Scheduled(cron="0 20 18 * * *") // 매일 오전 01(새벽)시에 실행 ( 9시간 빼야 함 )
+    @Scheduled(cron="0 0 1 * * *") // 매일 오전 01(새벽)시에 실행
     public void 서버정보백업_스케줄러() {
         log.info("[ 암스스케쥴러 :: 서버정보백업_스케줄러 ] 동작 : {}", Calendar.getInstance().getTime());
-        ResponseEntity 결과 = 엔진통신기.서버정보백업_스케줄러();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> 결과 = restTemplate.exchange("http://engine-fire:33333/engine/serverinfo/backup/scheduler", HttpMethod.POST, null, String.class);
         log.info(결과.toString());
     }
 
     @Scheduled(cron="0 30 1 * * *") // 매일 오전 01(새벽)시 30분에 실행
     public void 지라이슈_인덱스백업() {
         log.info("[ 암스스케쥴러 :: 지라이슈_인덱스백업 ] 동작 : {}", Calendar.getInstance().getTime());
-        ResponseEntity 결과 = 엔진통신기.지라이슈_인덱스백업();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> 결과 = restTemplate.exchange("http://engine-fire:33333/engine/jira/0/issue/index/backup", HttpMethod.POST, null, String.class);
         log.info(결과.toString());
     }
 
     @Scheduled(cron="0 0 3 * * *") // 매일 오전 03(새벽)시에 실행
     public void 각_제품서비스_별_요구사항이슈_조회_및_ES저장() {
         log.info("[ 암스스케쥴러 :: 각_제품서비스_별_요구사항이슈_조회_및_ES저장 ] 동작 : {}", Calendar.getInstance().getTime());
-        ResponseEntity 결과 = 백엔드코어통신기.각_제품서비스_별_요구사항이슈_조회_및_ES저장();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> 결과 = restTemplate.exchange("http://backend-core:31313/arms/scheduler/pdservice/reqstatus/loadToES", HttpMethod.GET, null, String.class);
         log.info(결과.toString());
     }
 
     @Scheduled(cron="0 0 6 * * *") // 매일 오전 06(새벽)시에 실행
     public void 각_제품서비스_별_요구사항_Status_업데이트_From_ES() {
         log.info("[ 암스스케쥴러 :: 각_제품서비스_별_요구사항_Status_업데이트_From_ES ] 동작 : {}", Calendar.getInstance().getTime());
-        ResponseEntity 결과 = 백엔드코어통신기.각_제품서비스_별_요구사항_Status_업데이트_From_ES();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> 결과 = restTemplate.exchange("http://backend-core:31313/engine/jira/0/issue/index/backup", HttpMethod.GET, null, String.class);
         log.info(결과.toString());
     }
 }
