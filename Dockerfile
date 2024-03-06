8c38536f9c9020d859f3429255f8690616a1d038
dmlee-mz <dmlee@mz.co.kr>
2024-03-06 20:36:35 +0900

    feat: [ARMSQA-19] #comment ✅ APM 설정 추가 - 24.03.06 #close #time 1h +review SR @313devops
    추신 : 맨앞에 붙일 수 있는 구분
    fix : Bug 류 처리 시 ( patch version up )
    feat : 신규 기능 류 처리 시 ( minor version up )
    perf : 메이저 기능 류 처리 시 ( major version up )
    
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,10 @@ MAINTAINER 313DEVGRP <313@313.co.kr>
 RUN apt-get update
 RUN apt-get -y -q install libpcap0.8 wget procps
 
-#RUN wget http://www.313.co.kr/nexus/content/groups/public/313devgrp/elastic-apm-agent/1.18.1/elastic-apm-agent-1.18.1.jar
-#RUN mv elastic-apm-agent-1.18.1.jar ./elastic-apm-agent.jar
+RUN wget http://www.313.co.kr/nexus/repository/ple-releases/313devgrp/elastic-apm-agent/1.47.1/elastic-apm-agent-1.47.1.jar
+RUN mv elastic-apm-agent-1.47.1.jar ./elastic-apm-agent.jar
 
 VOLUME /tmp
-
-COPY elastic-apm-agent-1.47.1.jar elastic-apm-agent-1.47.1.jar
 
 ARG ENTRY_FILE
 COPY ${ENTRY_FILE} docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 GC_OPTS=${GC_OPTS:="-XX:+UseNUMA -XX:+UseG1GC"}
 MEM_OPTS=${MEM_OPTS:="-Xms2048m -Xmx2048m"}
 NET_OPTS=${NET_OPTS:="-Dsun.net.inetaddr.ttl=0 -Dsun.net.inetaddr.negative.ttl=0 -Djava.net.preferIPv4Stack=true"}
-MONITOR_ELK_OPTS="-javaagent:/elastic-apm-agent-1.47.1.jar -Delastic.apm.service_name=middle-proxy -Delastic.apm.application_packages=com.arms -Delastic.apm.server_urls=http://192.168.25.31:8200"
+MONITOR_ELK_OPTS="-javaagent:/elastic-apm-agent.jar -Delastic.apm.service_name=middle-proxy -Delastic.apm.application_packages=com.arms -Delastic.apm.server_urls=http://192.168.25.31:8200"
 
 JAVA_OPTS=${JAVA_OPTS:="-server $GC_OPTS $MEM_OPTS $NET_OPTS $MONITOR_ELK_OPTS"}
 
