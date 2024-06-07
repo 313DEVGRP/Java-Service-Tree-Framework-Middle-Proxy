9571d72158d28a8e7b7fc216a626df9353dd7a23
sevoon0909 <sevoon0909@icloud.com>
2024-06-07 21:26:48 +0900

    feat : [JIRA-7] #comment 키클락 권한 지정 적용 #close #time 1h +review SR @sevoon0909
    [Middle-Proxy::Java-Service-Tree-Framework(JSTF)::mingyulee ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/config/security/SecurityConfiguration.java
+++ src/main/java/com/arms/config/security/SecurityConfiguration.java
@@ -1,5 +1,13 @@
 package com.arms.config.security;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.arms.config.handler.AuthSuccessHandler;
 import com.arms.config.handler.KeycloakLogoutHandler;
 import com.arms.config.handler.component.AuthSuccessAfterDuplicateUserRemove;
@@ -13,17 +21,28 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcReactiveOAuth2UserService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
+@Slf4j
 public class SecurityConfiguration {
 
     @Value("${spring.security.auth.success.redirect-url}")
@@ -91,6 +110,56 @@ public class SecurityConfiguration {
                         }))
                 )
                 .build();
+    }
+
+
+    @Bean
+    public ReactiveOAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        final OidcReactiveOAuth2UserService delegate = new OidcReactiveOAuth2UserService();
+
+        return (userRequest) -> {
+            // Delegate to the default implementation for loading a user
+            return delegate.loadUser(userRequest)
+                .doOnError(err -> log.info("Err: ", err.getCause()))
+                .doOnSuccess(user -> log.info("Auths " + user.getAuthorities()))
+                .map(user -> {
+                    Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+                    user.getAuthorities().forEach(authority -> {
+                        if (authority instanceof OidcUserAuthority) {
+                            OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
+                            mappedAuthorities.addAll(extractAuthorityFromClaims(oidcUserAuthority.getUserInfo().getClaims()));
+                        }
+                    });
+
+                    return new DefaultOidcUser(mappedAuthorities, user.getIdToken(), user.getUserInfo());
+                });
+        };
+    }
+
+    private List<GrantedAuthority> extractAuthorityFromClaims(Map<String, Object> claims) {
+        return mapRolesToGrantedAuthorities(getRolesFromClaims(claims));
+    }
+
+    private Collection<String> getRolesFromClaims(Map<String, Object> claims) {
+
+        LinkedHashMap<String, LinkedHashMap<String,List<String>>> realm_access = ((LinkedHashMap)claims.get("realm_access"));
+
+        if(realm_access==null){
+            return List.of("ROLE_USER");
+        }
+
+        if(realm_access.get("roles")==null){
+            return List.of("ROLE_USER");
+        }
+
+        return (Collection<String>) realm_access.get("roles");
+
+    }
+
+    private  List<GrantedAuthority> mapRolesToGrantedAuthorities(Collection<String> roles) {
+        return roles.stream().filter(role -> role.startsWith("ROLE_")).map(SimpleGrantedAuthority::new).collect(
+            Collectors.toList());
     }
 
 }
