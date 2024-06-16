c80d4db1eb6a8dc96fa0730bfc111e4b71c8042e
dmlee-mz <313cokr@gmail.com>
2024-06-16 09:55:07 +0900

    feat: [KAN-99] #comment ✅ 소나 큐브 대응 : redis connection - 24.06.16 #close #time 1h +review SR @313devops
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [IssueTracker] http://www.313.co.kr:3000
    [IssueTracker] http://www.313.co.kr/gitlab
    
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Gateway Traefik] http://www.313.co.kr/dashboard/
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [Oracle EM] https://www.313.co.kr:5500/em
    [MysqlDB Admin] http://www.313.co.kr/phpmyadmin/
    [PostgresDB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    [Analysis] http://www.313.co.kr/php/matomo/
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    
    [Opensource-License] http://www.313.co.kr/313DEVGRP-Java-Service-Tree-Framework-Middle-Proxy/build/licenses/index.html
    [Swagger] http://www.313.co.kr/middle-proxy-api/swagger-ui/

--- src/main/java/com/arms/util/redisrepo/CustomRedisTemplateImpl.java
+++ src/main/java/com/arms/util/redisrepo/CustomRedisTemplateImpl.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Component;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -15,13 +17,24 @@ import java.util.stream.Collectors;
 public class CustomRedisTemplateImpl implements CustomRedisTemplate{
 
     private final RedisTemplate<String,Object> redisTemplate;
+
+
     @Override
     public List<String> scan(String id) {
-        return redisTemplate.getConnectionFactory().getConnection()
-                .scan(ScanOptions.scanOptions().match(id).count(200).build())
-                .stream()
-                .map(a -> new KeyName(new String(a, StandardCharsets.UTF_8)).get())
-                .collect(Collectors.toList());
+        RedisConnectionFactory connectionFactory = redisTemplate.getConnectionFactory();
+        if (connectionFactory == null) {
+            throw new IllegalStateException("RedisConnectionFactory is not available");
+        }
+
+        try (RedisConnection connection = connectionFactory.getConnection()) {
+            return connection.scan(ScanOptions.scanOptions().match(id).count(200).build())
+                    .stream()
+                    .map(a -> new KeyName(new String(a, StandardCharsets.UTF_8)).get())
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            // 예외 처리: 로그를 남기거나 다른 방식으로 처리할 수 있습니다.
+            throw new RuntimeException("Error during scanning Redis keys", e);
+        }
     }
 
 }
