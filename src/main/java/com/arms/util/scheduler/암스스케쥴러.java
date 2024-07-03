574d12948b0f141da0279fe8dce0cb11c063f6e3
mingyu09 <mklee@mz.co.kr>
2024-07-03 17:45:38 +0900

    feat: [ARMSQA-19] #comment 스케쥴러에 Async 어노테이션 추가 #close #time 1h +review SR @sevoon0909
    [ Middle-Proxy::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/util/scheduler/암스스케쥴러.java
+++ src/main/java/com/arms/util/scheduler/암스스케쥴러.java
@@ -7,8 +7,10 @@ import com.arms.util.external_communicate.*;
 import com.arms.util.external_communicate.vo.서버정보_엔티티;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.util.Calendar;
 
@@ -24,7 +26,8 @@ public class 암스스케쥴러 {
     private final SlackNotificationService slackNotificationService;
 
     @LogAndSlackNotify
-    @Scheduled(cron="0 0 1 * * *") // 매일 오전 01(새벽)시에 실행
+    @Scheduled(cron="0 0 1 * * *") // 매일 오전 01(새벽)시에
+    @Async
     public void 서버정보백업_스케줄러() {
         Iterable<서버정보_엔티티> 결과 = 내부통신기.서버정보백업_스케줄러();
         String resultMessage = String.format("[ 암스스케쥴러 :: 서버정보백업_스케줄러 ] 결과 : %s", Calendar.getInstance().getTime()) + "\n\n" + 결과.toString();
@@ -35,6 +38,7 @@ public class 암스스케쥴러 {
     // @Scheduled(cron="0 30 1 * * *") // 매일 오전 01(새벽)시 30분에 실행
     // 엘라스틱 서치 롤링 인덱스를 사용 하기 때문에 자동 백업 중단 24.02.11
     @LogAndSlackNotify
+    @Async
     public void 지라이슈_인덱스백업() {
         boolean 결과 = 내부통신기.지라이슈_인덱스백업();
         if(결과){
@@ -46,6 +50,7 @@ public class 암스스케쥴러 {
 
     // 최초 한번 실행은 직접 실행으로 한다.
     @LogAndSlackNotify
+    @Async
     public void 각_제품서비스_별_요구사항이슈_조회_및_ES저장() {
         String 결과 = 내부통신기.각_제품서비스_별_요구사항이슈_조회_및_ES저장();
         log.info(결과);
@@ -53,6 +58,7 @@ public class 암스스케쥴러 {
 
     @LogAndSlackNotify
     @Scheduled(cron="0 0 3 * * *") // 매일 오전 03(새벽)시에 실행
+    @Async
     public void 각_제품서비스_별_요구사항_증분이슈_조회_및_ES저장() {
         String 결과 = 내부통신기.각_제품서비스_별_요구사항_증분이슈_조회_및_ES저장();
         log.info(결과);
@@ -60,11 +66,13 @@ public class 암스스케쥴러 {
 
     @LogAndSlackNotify
     @Scheduled(cron="0 0 6 * * *") // 매일 오전 06(새벽)시에 실행
+    @Async
     public void 각_제품서비스_별_요구사항_Status_업데이트_From_ES() {
         String 결과 = 내부통신기.각_제품서비스_별_요구사항_Status_업데이트_From_ES();
         log.info(결과);
     }
 
+    @Async
     @Scheduled(fixedDelay = 45000, initialDelay = 10000)
     public void 커넥션_상태_유지() {
         log.info("커넥션_상태_유지");
@@ -72,7 +80,8 @@ public class 암스스케쥴러 {
     }
 
     @LogAndSlackNotify
-    @Scheduled(cron="0 0 9,12,15,18,21 * * *") // 매일 09, 12,시, 15시, 18시, 21시 동작
+    @Scheduled(fixedDelay=10000) // 매일 09, 12,시, 15시, 18시, 21시 동작
+    @Async
     public void 각_제품서비스_별_생성실패한_ALM_요구사항_이슈_재생성() {
         String 결과 = 내부통신기.각_제품서비스_별_생성실패한_ALM_요구사항_이슈_재생성();
         log.info(결과);
